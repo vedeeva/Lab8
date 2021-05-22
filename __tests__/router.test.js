394cca3e60d17d5ebc852c3b6cf2eff26797509e
vedeeva <victoria.edeeva@gmail.com>
2021-05-22 12:53:29 -0700

    part1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,18 @@
-/**
- * @jest-environment jsdom
- */
+import {pushToHistory} from '../scripts/router.js';
 
+describe('push to history', () => {
+    test('settings', () => {
+        expect(pushToHistory('settings', 0).state).toEqual({ page: 'settings' });
+    });
+  
+    test('entry', () => {
+        expect(pushToHistory('entry', 1).state).toEqual({page: 'entry1'});
+      });
+
+    test('length', () => {
+        expect(pushToHistory('entry', 2).length).toEqual(history.length);
+    });
+    test('default', () => {
+        expect(pushToHistory('', 3).state).toEqual({});
+    });
+  });
